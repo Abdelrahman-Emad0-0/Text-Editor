efa472bf9facfc20ba58171ea3671979aeeb8ffa
EngSeif <eng.seifmohamed2004@gmail.com>
2025-05-04 00:14:30 +0300

    Add Basic Websocket

---
+++ backend/src/main/java/com/editor/backend/config/WebSocketConfig.java
@@ -0,0 +1,24 @@
+package com.editor.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        // Enable a simple message broker with the destination "/topic"
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // * define the WebSocket endpoint where clients will actually connect.
+        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
+    }
+}
---
+++ backend/src/main/java/com/editor/backend/service/DocumentWebSocketController.java
@@ -0,0 +1,14 @@
+package com.editor.backend.service;
+
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+
+public class DocumentWebSocketController {
+    
+    @MessageMapping("/updateDocument/{documentId}")
+    @SendTo("/topic/Document/{documentId}")
+    public void updateScore(@DestinationVariable String documentId) {
+        // TODO : Add Document Updated Here
+    }
+}
