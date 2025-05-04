2542573a89d3bb3255e3d70a56e4dcd77773a45c
EngSeif <eng.seifmohamed2004@gmail.com>
2025-05-04 21:24:50 +0300

    fix user and document session

--- backend/src/main/java/com/editor/backend/model/User.java
+++ backend/src/main/java/com/editor/backend/model/User.java
@@ -1,8 +1,15 @@
 package com.editor.backend.model;
+import java.util.UUID;
 
 public class User {
     private String userId;
     private String displayName;
     private String role; // Editor or Viewer
     
+    public User(String Role) {
+        this.userId = UUID.randomUUID().toString();
+        if (Role.equalsIgnoreCase("editor") || Role.equalsIgnoreCase("viewer")) {
+            this.role = Role;
+        }
+    }
 }
